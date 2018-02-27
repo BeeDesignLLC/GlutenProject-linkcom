89fdd9b255ae8bc109a5d34f833145fe036b3155
Brandon Bayer <b@bayer.ws>
2018-02-26 21:55:06 -0500

    new: add walmart and test

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const {send} = require('micro')
 const {GraphQLClient} = require('graphql-request')
 const parse = require('url').parse
 const route = require('path-match')()
+const matchTest = route('/link/test')
 const matchOffer = route('/link/offer/:id')
 
 const client = new GraphQLClient(
@@ -18,6 +19,7 @@ const offerQuery = `query Offer($id: ID!){
 }`
 
 const thriveBase = 'http://www.kqzyfj.com/click-8542692-12982167?url='
+const walmartBase = 'http://linksynergy.walmart.com/deeplink?id=8loQjOHw*oo&mid=2149&murl='
 
 module.exports = async (req, res) => {
   res.setHeader('X-Robots-Tag', 'noindex, nofollow')
@@ -29,16 +31,26 @@ module.exports = async (req, res) => {
       id: offer.id,
     })
 
-    if (merchant === 'Thrive') {
-      Location = thriveBase + encodeURIComponent(url)
-    } else {
-      Location = url
+    switch (merchant) {
+      case 'Thrive':
+        Location = thriveBase + encodeURIComponent(url)
+        break
+      case 'Walmart':
+        Location = walmartBase + encodeURIComponent(url)
+        break
+      default:
+        Location = url
     }
   }
 
   if (Location) {
     res.writeHead(302, {Location})
     return res.end()
+  }
+
+  const test = matchTest(parse(req.url).pathname)
+  if (test !== false) {
+    return send(res, 200)
   }
 
   return send(res, 404, '<h1>Link not found :(</h1>')
